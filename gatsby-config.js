69e6a5b03eacd910e1087e3bb12d0f24c0de9342
Tarun Varshney <imtarunvarshney@gmail.com>
2020-04-29 15:08:33 +0530

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Tarun Varshney`,
+      summary: `, a final year Computer Science student passionate about technology, building things and self-improvement.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
